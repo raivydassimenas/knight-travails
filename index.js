be7f6842ff3e69bdbd3f78f356b5560db78dbca8
Raivydas Šimėnas <raivydas.simenas@proton.me>
2023-10-25 18:21:59 +0300

    Attempt to BFS.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const board = [];
+let board = [];
 for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
         board.push([i, j]);
@@ -7,8 +7,6 @@ for (let i = 0; i < 8; i++) {
 
 const initialSquare = [Math.floor(Math.random() * 8), Math.floor(Math.random() * 8)];
 const finalSquare = [Math.floor(Math.random() * 8), Math.floor(Math.random() * 8)];
-
-const shortestPath = [initialSquare];
 
 function availableMoves(currSquare, board) {
     const possibilities = [
@@ -26,3 +24,42 @@ function availableMoves(currSquare, board) {
 }
 
 console.log(availableMoves([0, 0], board));
+
+let availableNodes = [initialSquare];
+const visited = [];
+
+function breadthFirstSearch({currNode, parent} = {currNode: initialSquare, parent: null}) {
+    if (currNode === finalSquare) {
+        console.log("The path has been found.");
+        console.log(getShortestPath(visited, currNode));
+        return;
+    }
+
+    if (availableNodes.length === 0) {
+        console.log(`The path from ${initialSquare} to ${finalSquare} is impossible.`);
+        return;
+    }
+
+    visited.push({currNode, parent});
+
+    const newNodes = availableMoves(currNode, board);
+    board = board.filter(item => !newNodes.includes(item));
+    availableNodes = [...availableNodes, ...newNodes];
+
+    const newParent = currNode;
+    const newCurrNode = availableNodes.shift();
+
+    breadthFirstSearch({currNode: newCurrNode, parent: newParent});
+}
+
+function getShortestPath(visited, currNode) {
+    const path = [];
+    while (currNode !== null) {
+        visited.unshift(currNode);
+        currNode = visited.filter(item => item.currNode === currNode)[0].parent;
+    }
+
+    return path;
+}
+
+breadthFirstSearch();
